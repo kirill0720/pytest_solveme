5ce11a2ea6044cfef2a431f0d593ab4aaddfbb79
kirill0720 <knete-87@mail.ru>
2023-03-05 10:18:05 +0300

    Update test

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,8 +1,10 @@
 import requests
 
-from configuration import SERVICE_URL
+from jsonschema import validate
 
+from configuration import SERVICE_URL
 from src.enums.global_enums import GlobalErrorMessages
+from src.schemas.post import POST_SCHEMA
 
 
 def test_getting_posts():
@@ -11,3 +13,5 @@ def test_getting_posts():
 
     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
     assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT
+    for item in received_posts:
+        validate(item, POST_SCHEMA)
