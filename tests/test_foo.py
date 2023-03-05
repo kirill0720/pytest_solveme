a776c98ed05f83555b7a5a42cfdee61e6ccaafdb
kirill0720 <knete-87@mail.ru>
2023-03-05 10:03:11 +0300

    Update first test

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -2,8 +2,12 @@ import requests
 
 from configuration import SERVICE_URL
 
+from src.enums.global_enums import GlobalErrorMessages
+
 
 def test_getting_posts():
     response = requests.get(url=SERVICE_URL)
-    assert response.status_code == 200
-    print(response.json())
+    received_posts = response.json()
+
+    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+    assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT
