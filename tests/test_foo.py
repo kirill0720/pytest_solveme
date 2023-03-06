8c862db79d061090171ae068329092f7f3a22341
kirill0720 <knete-87@mail.ru>
2023-03-06 17:29:08 +0300

    Improve validation and add jsonschema

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,17 +1,11 @@
 import requests
 
-from jsonschema import validate
-
 from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
+from src.baseclasses.responce import Response
 from src.schemas.post import POST_SCHEMA
 
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    received_posts = response.json()
-
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT
-    for item in received_posts:
-        validate(item, POST_SCHEMA)
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+    response.assert_status_code(200).validate(POST_SCHEMA)
