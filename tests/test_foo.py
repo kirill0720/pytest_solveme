3f8c508c959e51ed2d16098cfd7eea3eb3be33ab
kirill0720 <knete-87@mail.ru>
2023-03-08 01:28:58 +0300

    Add docstring to the test

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -6,4 +6,5 @@ from src.schemas.user import User
 
 @pytest.mark.development
 def test_getting_users_list(test_response):
+    """Test status 200 and data is valid."""
     Response(test_response).assert_status_code(200).validate(User)
