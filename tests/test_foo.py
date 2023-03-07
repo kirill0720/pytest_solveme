38bfd35c710594edb16cac4bd0a3bdff61f474a9
kirill0720 <knete-87@mail.ru>
2023-03-08 00:52:48 +0300

    Add pytest.ini and use development marker for the test

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,9 +1,9 @@
-import requests
+import pytest
 
-from configuration import SERVICE_URL
 from src.baseclasses.response import Response
 from src.schemas.user import User
 
 
+@pytest.mark.development
 def test_getting_users_list(test_response):
     Response(test_response).assert_status_code(200).validate(User)
